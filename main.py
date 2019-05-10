c38e227c746cc14f67c7df8646722fc8d1e41854
Aaron Barratt <aarontbarratt@gmail.com>
2019-05-10 15:41:50 +0100

    Adds all content lines to list, removes duplicates from list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-import time
 import urllib.request
 
 logging.basicConfig(level=logging.DEBUG)
@@ -7,12 +6,13 @@ logging.basicConfig(level=logging.DEBUG)
 # URL = input()
 # will grab the profile image if a link to a profile is supplied
 # will pull the first image from a gallery
-# will pull 
-URL = r"https://www.instagram.com/lukejinks/"
+# will pull
+URL = r"https://www.instagram.com/p/BxQmFKPl3tt/?utm_source=ig_web_copy_link"
 
 CONTENT_STUB = "content=\"https://scontent"
 response = ''
 image_link = ''
+image_links = []
 
 export_path = r"C:\temp"
 export_file_name = ''
@@ -31,18 +31,12 @@ html = html.split()
 
 for line in html:
     if CONTENT_STUB in line:
-        image_link = line
+        image_link = line.strip("content=").strip("\"")
+        image_links.append(image_link)
 
-image_link = image_link.strip("content=").strip("\"")
+# remove all duplicated in image_links
+image_links = list(dict.fromkeys(image_links))
+print(image_links)
 
-logging.info(" Image Found: " + image_link)
-
-if "mp4" in image_link:
-    export_file_extension = "mp4"
-elif "jpg" in image_link:
-    export_file_extension = "jpg"
-
-export_full_path = export_path + "\\" + str(time.time()) + "." + export_file_extension
-
-logging.info(" Export: " + export_full_path)
-urllib.request.urlretrieve(image_link, export_full_path)
+for link in image_links:
+    print(link)
